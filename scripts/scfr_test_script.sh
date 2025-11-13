26786a76678f39d29452e7f99586223eea91e833
Aswin S Soman <ceglab3@gmail.com>
2025-11-13 15:39:23 +0530

    Update scfr_test_script.sh

--- scripts/scfr_test_script.sh
+++ scripts/scfr_test_script.sh
@@ -1,18 +1,114 @@
+####################################################################################################################################################################################################################################################################################################################
+#
+####################################################################################################################################################################################################################################################################################################################
 
+####################################################################################################################################################################################################################################################################################################################
 
+#Doownload github repository
+cd /media/aswin/SCFR
+wget https://github.com/ceglab/SCFR/archive/refs/heads/main.zip
+unzip main.zip
+
+#Create folders
+cd /media/aswin/SCFR/SCFR-main
+for species in human bonobo chimpanzee gorilla borangutan sorangutan gibbon
+do
+echo $species
+mkdir -p chrs/"$species"
+mkdir -p SCFR/"$species"
+mkdir -p SCFR/gaps/"$species"
+mkdir -p genes/"$species"
+mkdir -p GC/"$species"
+mkdir -p SCFR_all
+mkdir -p scripts
+done
+
+####################################################################################################################################################################################################################################################################################################################
+#Quality check
 
 cd /media/aswin/SCFR/SCFR-main
+mkdir -p QC/genome_metadata
 
-time while read species
+#Get genome metadata using datasets (0m18.381s)
+cd /media/aswin/SCFR/SCFR-main/QC
+while read i
 do
-genome=$(echo $species | awk '{print$1}')
-name=$(echo $species | awk '{print$2}')
-echo ">"$genome $name
-#Download
-time datasets download genome accession $genome --include genome,gtf,seq-report --filename $genome".zip"
-unset genome name
+echo $i
+i1=$(echo $i | awk '{print$1}')
+i2=$(echo $i | awk '{print$2}')
+datasets summary genome accession "$i1" --report sequence --as-json-lines > genome_metadata/$i2"_"$i1"_genome_metadata.json"
+cat genome_metadata/$i2"_"$i1"_genome_metadata.json" | json2xml | xtract -pattern opt -def "-" -element assembly_accession assembly_unit assigned_molecule_location_type chr_name gc_count gc_percent genbank_accession length refseq_accession role sequence_name | tr " " "_" | sed '1i assembly_accession assembly_unit assigned_molecule_location_type chr_name gc_count gc_percent genbank_accession length refseq_accession role sequence_name' | column -t > genome_metadata/$i2"_"$i1"_genome_metadata.txt"
+unset i1 i2
+done < genome_accessions
+
+cd /media/aswin/SCFR/SCFR-main/QC/genome_metadata
+find . -name "*_genome_metadata.txt" | xargs -n1 sh -c 'awk "{print\$9,\$8}" $0' | grep -v "refseq_accession" | sed '1i refseq_accession Length' > all_species_chr_length
+
+####################################################################################################################################################################################################################################################################################################################
+#Download all the seven primate genomes
+
+cd /media/aswin/SCFR/SCFR-main
+time while read species
+  do
+  genome=$(echo $species | awk '{print$1}')
+  name=$(echo $species | awk '{print$2}')
+  echo ">"$genome $name
+  #Download genome & annotation files
+  time datasets download genome accession $genome --include genome,gtf,seq-report --filename $genome".zip"
+  unzip -o $genome".zip"
+  cd ncbi_dataset/data/$genome
+  #Get accessions
+  cat sequence_report.jsonl | json2xml | xtract -pattern opt -def "-"  -element genbankAccession refseqAccession | awk '$2!="-"' > $genome"_genbank_refseq_accession"
+  #Add refseq accesion in genome
+  time awk 'NR==FNR {map[$1]=$2; next}  /^>/ {hdr=$0; for(k in map) {if(hdr ~ ">"k"($|[[:space:]])") {sub(">"k, ">"map[k]" "k); break}}} {print}' $genome"_genbank_refseq_accession" $genome*".fna" > $genome".fna"
+  #Get chromosome fasta files
+  outdir="/media/aswin/SCFR/SCFR-main/chrs/$name"
+  awk -v outdir="$outdir" '/^>/ { if (out) close(out) ; out = outdir "/" substr($1,2) ".fasta" } { print >> out }' "${genome}.fna"
+  unset genome name
+  cd /media/aswin/SCFR/SCFR-main
 done < QC/genome_accessions
 
+
+####################################################################################################################################################################################################################################################################################################################
+#QC: Check length of fetched genome sequences
+
+cd /media/aswin/SCFR/SCFR-main/chrs
+time for species in human bonobo chimpanzee gorilla borangutan sorangutan gibbon
+     do
+     cd $species
+     for chr in $(ls *.fasta)
+     do
+     length=$(myfasta -l $chr | awk '{print$1,$NF}')
+     echo $species $ch $length
+     unset length
+     done
+     unset chr
+     cd /media/aswin/SCFR/SCFR-main/chrs
+done | sed '1i Species chromosome length' | column -t > /media/aswin/SCFR/SCFR-main/QC/genome_metadata/fetched_length
+
+cd /media/aswin/SCFR/SCFR-main/QC/genome_metadata
+for chr in $(awk '{print$1}' all_species_chr_length | egrep -v "refseq_accession|-")
+     do
+     m1=$(awk -v a="$chr" '$1==a' all_species_chr_length)
+     m2=$(awk -v a="$chr" '$2==a' fetched_length)
+     echo $m1 $m2
+     unset m1 m2
+done | awk '{if($2==$5) print$0,"same"; else print$0,"different"}' |  sed '1i ncbi_accession ncbi_length species fetched_accession fetched_length Compare_length' | column -t > compare_chromosome_length
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################################################################################################################################################################################################################################################
+#DRAFT SCRIPTS
 
 #Download
 time datasets download genome accession GCA_028878055.3 --include genome,gtf,seq-report --filename GCA_028878055.3.zip
@@ -26,8 +122,6 @@ cat sequence_report.jsonl | json2xml | xtract -pattern opt -def "-"  -element ge
 #Add refseq accesion in genome
 awk 'NR==FNR {map[$1]=$2; next}  /^>/ {hdr=$0; for(k in map) {if(hdr ~ ">"k"($|[[:space:]])") {sub(">"k, ">"map[k]" "k); break}}} {print}' genbank_refseq_accession test2.fa > test2_updated.fna
 
-
-
 #Add refseq accesion in genome
 time awk 'NR==FNR {map[$1]=$2; next} /^>/ {hdr=$0; for(k in map) {if(hdr~">"k"($|[[:space:]])") {sub(">"k, ">"map[k]" "k); break}}} {print}' genbank_refseq_accession test1.fa > tmp && mv tmp test1.fa
 
@@ -37,9 +131,6 @@ time gawk -iinplace 'NR==FNR {map[$1]=$2; next} /^>/ {id=substr($1,2); if (id in
 awk 'NR==FNR {map[$1]=$2;next} /^>/{hdr=$0 for(k in map) {if(hdr~">"k" ($|[[:space:]])") {sub(">"k, ">"map[k]" "k) break }}} {print}' genbank_refseq_accession GCA_028858775.3_NHGRI_mPanTro3-v2.1_pri_genomic.fna \
 > updated.fna
 
-
-
-#
 start_time=$(date +%s)
 while read chr 
 do
