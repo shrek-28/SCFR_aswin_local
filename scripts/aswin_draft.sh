4098a0cea485a653b89a563d6f50a6c108109a71
Aswin S Soman <ceglab3@gmail.com>
2025-11-28 14:11:07 +0530

    Update aswin_draft.sh

--- scripts/aswin_draft.sh
+++ scripts/aswin_draft.sh
@@ -1,9 +1,9 @@
+#############################################################################################################################################################################################################################################################################################################
 
-cd /media/aswin/SCFR/SCFR-main/SCFR_summaries
+
 
 mkdir /media/aswin/SCFR/SCFR-main/SCFR_summaries/circos_plot
 cd /media/aswin/SCFR/SCFR-main/SCFR_summaries/circos_plot
-
 
 cd /media/aswin/SCFR/SCFR-main/SCFR_summaries/circos_plot
 find /media/aswin/SCFR/SCFR-main/genes/$species/ -name "*merged*" | xargs -n1 sh -c 'cp $0 /media/aswin/SCFR/SCFR-main/SCFR_summaries/circos_plot/'
@@ -13,14 +13,18 @@ cp /media/aswin/SCFR/SCFR-main/genome_sizes/$species".genome" .
 
 
 ##########
+#View SCFR length filtered summary in window-wise
 
+for i in $(awk -F "," '{print$2}' window_wise_all_species_scfr_coding_stats.csv | grep -v Window | sort -u)
+do
+j=$(for s in human bonobo chimpanzee gorilla borangutan sorangutan gibbon
+do
+awk -F "," -v a="$i" '$2==a {print$1,$3}' window_wise_all_species_scfr_coding_stats.csv | grep $s | awk '{print$2}'
+done)
+echo $i $j
+unset j
+done | sed '1i Window human bonobo chimpanzee gorilla borangutan sorangutan gibbon' | awk 'NR==1 {print; next} {for(i=2;i<=NF;i++) $i=$i/1000000; print}' OFS="\t" | column -t
 
-find /media/aswin/SCFR/SCFR-main/SCFR_lists/ -name "human*" | egrep -v "in_non_coding" | xargs -n1 sh -c 'cp $0 .'
-cp /media/aswin/SCFR/SCFR-main/genes/human/human_cds_merged.bed
-
-
-find /media/aswin/SCFR/SCFR-main/SCFR_lists/ -name "human*" | egrep -v "in_non_coding" 
-/media/aswin/SCFR/SCFR-main/genome_sizes/human.genome
-/media/aswin/SCFR/SCFR-main/genes/human/human_cds_merged.bed
-
+#View higher strand asymmetry
+sed 's/ /_/g' genome_reports/GCA_029289425.3_bonobo.tsv | awk -F"\t" -v OFS="\t" '{ for(N=1; N<=NF; N++) if($N=="") $N="-" } 1' | awk '{print$9,$3,$4,$5,$11}' | sort -k5,5n | colnum.sh 
 
